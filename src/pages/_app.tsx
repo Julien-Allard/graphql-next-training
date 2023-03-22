89c00b1d5599d4d47f20cdd9eaa5290bfc76de3d
Julien-Allard <julalldev@gmail.com>
2023-03-22 14:05:50 +0100

    Fix/Trying something

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ import '../pages/locations/by-id/locationbyid.scss';
 import type { AppProps } from 'next/app';
 
 export const client = new ApolloClient({
-   uri: 'https://graphql-server-training.herokuapp.com/',
+   uri: 'http://localhost:3000/api/graphql',
    cache: new InMemoryCache(),
 });
 
